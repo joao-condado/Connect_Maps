922e910fc3f35a202d31481d06efc66fd586e150
unknown <condado.carvalho@gmail.com>
2024-09-25 12:07:33 -0300

    Adição do model e controller

---
+++ src/main/java/com/gabriel/ltp/model/User.java
@@ -0,0 +1,24 @@
+package com.gabriel.ltp.model;
+
+public class User{
+
+    private String login;
+
+    public User(){
+
+    }
+
+    public User (String login){
+        this.login = login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    
+}
