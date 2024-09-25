b425abf35257654a09c9767d563cf851f50ad688
unknown <condado.carvalho@gmail.com>
2024-09-25 12:02:33 -0300

    Adição do projeto

---
+++ src/test/java/com/gabriel/ltp/LtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.ltp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
