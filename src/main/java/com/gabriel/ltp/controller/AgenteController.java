b7f37a29f6996df20e562749eb750136e3296f56
joao-condado <condado.carvalho@gmail.com>
2024-10-22 16:47:15 -0300

    Atualizações finais para resultar em Thunder Client

--- src/main/java/com/gabriel/ltp/controller/AgenteController.java
+++ src/main/java/com/gabriel/ltp/controller/AgenteController.java
@@ -2,16 +2,21 @@
 package com.gabriel.ltp.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.gabriel.ltp.model.Agente;
 
+@RestController
+@RequestMapping("/agente")
 public class AgenteController {
     
     @GetMapping("/agente/{numIdent}")
@@ -35,10 +40,10 @@ public class AgenteController {
         }
     }
 
-    @PostMapping("/agente/criarUsuarioAgente")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Agente endPoint3(@RequestBody Agente AgenteNovo){
-        return AgenteNovo;
+    @PostMapping("/criarUsuarioAgente")
+    // @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<String> criarUsuarioAgente(@RequestBody Agente agente){
+        return ResponseEntity.ok("Agente criado com sucesso! Função: " + agente.getFuncao());
     }
 
 
