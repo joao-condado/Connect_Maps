d05def1b7cf92df6bd018580af85188800aedfaf
JoaoBCamilo <joaobcgabriel@gmail.com>
2024-10-29 16:50:24 -0300

    Atualização das tabelas

--- src/main/java/com/gabriel/ltp/model/Agente.java
+++ src/main/java/com/gabriel/ltp/model/Agente.java
@@ -6,11 +6,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Entity
 @Data
 @NoArgsConstructor
+@Table(name = "AgCivil")
 public class Agente {
     
     @Id
--- src/main/java/com/gabriel/ltp/model/Cidadao.java
+++ src/main/java/com/gabriel/ltp/model/Cidadao.java
@@ -6,12 +6,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@Table(name = "Cid")
 public class Cidadao {
     
     @Id
