1ff1acebc8be7314d0d3eb9fd25ac810ff238a87
JoaoBCamilo <joaobcgabriel@gmail.com>
2024-10-31 19:36:19 -0300

    atualização repository

--- src/main/java/com/gabriel/ltp/repository/DenunciaRepository.java
+++ src/main/java/com/gabriel/ltp/repository/DenunciaRepository.java
@@ -1,5 +1,9 @@
 package com.gabriel.ltp.repository;
 
-public interface DenunciaRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.gabriel.ltp.model.Denuncia;
+
+public interface DenunciaRepository extends CrudRepository<Denuncia, Integer>{
 
 }
