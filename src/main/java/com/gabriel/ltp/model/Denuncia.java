5c5facc60cd96012cce58cd74fb26402d2b46bf3
JoaoBCamilo <joaobcgabriel@gmail.com>
2024-10-31 20:21:02 -0300

    relacionamento many to one

--- src/main/java/com/gabriel/ltp/model/Denuncia.java
+++ src/main/java/com/gabriel/ltp/model/Denuncia.java
@@ -1,10 +1,14 @@
 package com.gabriel.ltp.model;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -31,6 +35,9 @@ public class Denuncia{
     )
     private int grauReRisco;
 
-   
-    
+    @ManyToOne
+    @JoinColumn(name = "Id_Cidadao")
+    private Cidadao cidadao;
+
+
 }
