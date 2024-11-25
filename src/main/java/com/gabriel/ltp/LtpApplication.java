1a51794a8c828a47b142ba26b042a7918b9e554f
joao-condado <condado.carvalho@gmail.com>
2024-11-25 09:06:46 -0300

    Adição a base para a conexão WEB

--- src/main/java/com/gabriel/ltp/LtpApplication.java
+++ src/main/java/com/gabriel/ltp/LtpApplication.java
@@ -2,12 +2,26 @@ package com.gabriel.ltp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LtpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LtpApplication.class, args);
+
+		System.out.println("ssss");
+
 	}
 
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+
+		@Override
+		public void addCorsMappings(CorsRegistry registry){
+			registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+		}
+	}
 }
