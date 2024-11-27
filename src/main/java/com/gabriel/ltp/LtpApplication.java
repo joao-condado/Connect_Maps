a7a8e1e453c09d551e30c9dcc02eb7d3e596c2cd
JoaoBCamilo <joaobcgabriel@gmail.com>
2024-11-27 20:36:51 -0300

    modified:   src/main/java/com/gabriel/ltp/LtpApplication.java

--- src/main/java/com/gabriel/ltp/LtpApplication.java
+++ src/main/java/com/gabriel/ltp/LtpApplication.java
@@ -21,7 +21,7 @@ public class LtpApplication {
 
 		@Override
 		public void addCorsMappings(CorsRegistry registry){
-			registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+			registry.addMapping("/**").allowedOrigins("http://localhost:3306");
 		}
 	}
 }
