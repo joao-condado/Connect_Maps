92b1db1796284dabc6838d6ab0c4d8818174ad39
JoaoBCamilo <joaobcgabriel@gmail.com>
2024-11-28 13:21:42 -0300

    modified:   src/main/java/com/gabriel/ltp/controller/AgenteController.java 	modified:   src/main/java/com/gabriel/ltp/controller/CidadaoController.java 	modified:   src/main/java/com/gabriel/ltp/controller/DenunciaController.java

--- src/main/java/com/gabriel/ltp/controller/AgenteController.java
+++ src/main/java/com/gabriel/ltp/controller/AgenteController.java
@@ -3,6 +3,7 @@ package com.gabriel.ltp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.gabriel.ltp.model.Agente;
 import com.gabriel.ltp.repository.AgenteRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/agente")
 public class AgenteController {
     
